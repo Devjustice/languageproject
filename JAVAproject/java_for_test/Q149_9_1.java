655ab21ada88a2cd3b765a1983e2d7073ca7dd8f
Devjustice <justice2638@hotmail.com>
2021-04-01 18:44:02 +0900

    비트연산자연습

---
+++ JAVAproject/java_for_test/Q149_9_1.java
@@ -0,0 +1,19 @@
+package java_for_test;
+
+public class Q149_9_1 {
+
+
+    public static void main(String args[]){
+
+
+        int c=1;
+
+        c= c<<4;
+        System.out.printf("%d",c);
+
+
+
+
+    }
+    
+}
